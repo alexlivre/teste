ebbfac5ff8ad7d4daf9fa6685dcdc55cd99cca32
alexlivre <alexbreno2005@gmail.com>
2026-03-28 08:31:47 -0300

    feat: add configuration file with secrets

---
+++ config-with-secrets.js
@@ -0,0 +1,17 @@
+// Configuration file
+const config = {
+  database: {
+    host: 'localhost',
+    password: 'supersecretpassword123',
+    user: 'admin'
+  },
+  api: {
+    key: 'sk-1234567890abcdefghijklmnop',
+    token: 'ghp_abcdefghijklmnopqrstuvwxyz123456'
+  },
+  aws: {
+    accessKeyId: 'AKIAIOSFODNN7EXAMPLE',
+    secretAccessKey: 'wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY'
+  }
+};
+module.exports = config;
